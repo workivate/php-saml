9ae64d406d967cc323de0363729b9d955ab01498
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2020-12-10 06:28:09 +0200

    Fix "Enable server certificate validation on this SSL/TLS connection"
    Ref: https://sonarcloud.io/project/issues?id=workivate_php-saml&issues=AXZK0C7lqpyGZVQmh2ws&open=AXZK0C7lqpyGZVQmh2ws

--- src/Saml2/IdPMetadataParser.php
+++ src/Saml2/IdPMetadataParser.php
@@ -46,7 +46,6 @@ class IdPMetadataParser
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
             curl_setopt($ch, CURLOPT_FAILONERROR, 1);
 
             $xml = curl_exec($ch);
